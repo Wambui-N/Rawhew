9aab6fdcda5baae8b092d37831063042d9dd7f1b
Wambui Ndungu <wambuin18@gmail.com>
2024-02-28 11:00:04 +0300

    Update Hero component styling

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -8,23 +8,10 @@ export const Hero = ({ title, img, alt }: HeroProps) => {
     <div className="my-[3vh] max-h-screen">
       <h1
         style={{ fontFamily: "myFontCalt" }}
-        className={`uppercase text-center text-8xl pt-10`}
+        className={`uppercase text-center lg:text-8xl md:text-7xl sm:text-6xl text-5xl py-10`}
       >
         {title}
       </h1>
-
-      <Link href="#">
-        <div className="-mt-24 relative left-[42.5vw] top-[13vh] bg-white rounded-full h-[15vw] w-[15vw]">
-          <div className="relative left-[1vw] top-[1vw] bg-primary rounded-full h-[13vw] w-[13vw] flex items-center place-content-center">
-            <p
-              style={{ fontFamily: "myFont" }}
-              className="Capitalize text-center text-md text-4xl self-start m-8 h-[6vw] w-[6vw]"
-            >
-              Give us a call
-            </p>
-          </div>
-        </div>
-      </Link>
 
       <Image
         className="object-cover max-h-[60vh]"
