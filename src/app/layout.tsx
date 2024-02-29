8ef4a173984f406d7c636007f190a5c30d4c23c4
Wambui Ndungu <wambuin18@gmail.com>
2024-02-29 11:16:48 +0300

    Add description to metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,12 @@ import { Raleway } from "next/font/google";
 import "./globals.css";
 import { Navbar, Footer } from "@/Components";
 
-
 const raleway = Raleway({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Rawhew Ranch Kenya",
+  description:
+    "A small intimate campsite bordering KWS Game Farm, a few meters from off the Naivasha – Kinangop Road. We offer high-quality pitches with a wide variety of facilities to enrich your camping experience. Our location away from the urban jungle and the busy highway gives our camp an adventurous sense of solitude and freedom.",
 };
 
 export default function RootLayout({
@@ -17,11 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${raleway.className} min-h-screen min-w-screen bg-white text-black`}>
+      <body
+        className={`${raleway.className} min-h-screen min-w-screen bg-white text-black`}
+      >
         <Navbar />
         {children}
         <Footer />
-        </body>
+      </body>
     </html>
   );
 }
