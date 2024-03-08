b45d7657fb63dad26f887fa827598bc1763684aa
Wambui Ndungu <wambuin18@gmail.com>
2024-03-08 11:33:43 +0300

    Update layout title and remove duplicate CSS import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
-import "./globals.css";
 import { Navbar, Footer } from "@/Components";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Rawhew Ranch Kenya",
+  title: "Rawhew Ranch",
   description:
     "A small intimate campsite bordering KWS Game Farm, a few meters from off the Naivasha – Kinangop Road. We offer high-quality pitches with a wide variety of facilities to enrich your camping experience. Our location away from the urban jungle and the busy highway gives our camp an adventurous sense of solitude and freedom.",
 };
@@ -18,13 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <title>Rawhew Ranch Kenya</title>
-        <meta
-          name="description"
-          content="A small intimate campsite bordering KWS Game Farm, a few meters from off the Naivasha – Kinangop Road. We offer high-quality pitches with a wide variety of facilities to enrich your camping experience. Our location away from the urban jungle and the busy highway gives our camp an adventurous sense of solitude and freedom."
-        ></meta>
-      </head>
       <body
         className={`${raleway.className} min-h-screen min-w-screen bg-white text-black`}
       >
